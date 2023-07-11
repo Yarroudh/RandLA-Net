363473e14d309edbb8c388b28617f117f7736272
Anass Yarroudh <ayarroudh@uliege.be>
2023-07-11 23:51:39 +0200

    Windows files

--- compile_op.bat
+++ compile_op.bat
@@ -1,7 +1,5 @@
 cd utils\nearest_neighbors
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
 python setup.py install --home="."
 
 cd ..\..\
--- utils/cpp_wrappers/compile_wrappers.bat
+++ utils/cpp_wrappers/compile_wrappers.bat
@@ -3,8 +3,6 @@
 REM Compile cpp subsampling
 cd cpp_subsampling
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
 python setup.py build_ext --inplace
 
 cd ..
